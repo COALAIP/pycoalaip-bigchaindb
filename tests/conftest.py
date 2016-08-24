f55b77f0bf75ff70cc5ab891eb641836044708fb
Brett Sun <qisheng.brett.sun@gmail.com>
2016-08-24 17:46:17 +0200

    (test): add initial tests for driver integration

---
+++ tests/conftest.py
@@ -0,0 +1,92 @@
+from os import environ
+
+from pytest import fixture
+
+
+@fixture
+def alice_signing_key():
+    return 'CT6nWhSyE7dF2znpx3vwXuceSrmeMy9ChBfi9U92HMSP'
+
+
+@fixture
+def alice_verifying_key():
+    return 'G7J7bXF8cqSrjrxUKwcF8tCriEKC5CgyPHmtGwUi4BK3'
+
+
+@fixture
+def alice_keypair(alice_signing_key, alice_verifying_key):
+    from bigchaindb_driver.crypto import CryptoKeypair
+    keypair = CryptoKeypair(alice_signing_key, alice_verifying_key)
+    return keypair
+
+
+@fixture
+def bob_signing_key():
+    return '4S1dzx3PSdMAfs59aBkQefPASizTs728HnhLNpYZWCad'
+
+
+@fixture
+def bob_verifying_key():
+    return '2dBVUoATxEzEqRdsi64AFsJnn2ywLCwnbNwW7K9BuVuS'
+
+
+@fixture
+def bob_keypair(bob_signing_key, bob_verifying_key):
+    from bigchaindb_driver.crypto import CryptoKeypair
+    keypair = CryptoKeypair(bob_signing_key, bob_verifying_key)
+    return keypair
+
+
+@fixture
+def bdb_host():
+    return environ.get('BDB_HOST', 'localhost')
+
+
+@fixture
+def bdb_port():
+    return environ.get('BDB_PORT', '9984')
+
+
+@fixture
+def bdb_node(bdb_host, bdb_port):
+    return 'http://{host}:{port}/api/v1'.format(host=bdb_host, port=bdb_port)
+
+
+@fixture
+def plugin(bdb_node):
+    from coalaip_bigchaindb import Plugin
+    return Plugin(bdb_node)
+
+
+@fixture
+def bdb_driver(bdb_node):
+    from bigchaindb_driver import BigchainDB
+    return BigchainDB(bdb_node)
+
+
+@fixture
+def manifestation_model_jsonld():
+    return {
+        '@context': 'http://schema.org/',
+        '@type': 'CreativeWork',
+        'name': 'Manifestation Title',
+        'creator': 'https://ipdb.foundation/api/transactions/12346789',
+    }
+
+
+@fixture
+def manifestation_model_json():
+    return {
+        'type': 'CreativeWork',
+        'name': 'Manifestation Title',
+        'creator': 'https://ipdb.foundation/api/transactions/12346789',
+    }
+
+
+@fixture
+def persisted_manifestation(bdb_driver, manifestation_model_jsonld,
+                            alice_keypair):
+    return bdb_driver.create(
+        manifestation_model_jsonld,
+        verifying_key=alice_keypair.vk,
+        signing_key=alice_keypair.sk)
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,28 +1,65 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-"""
-test_plugin
-----------------------------------
-
-Tests for `coalaip_bigchaindb` ledger plugin.
-"""
-
-import pytest
+from time import sleep
+from pytest import mark
 
 
-from coalaip_bigchaindb import plugin
+def test_init_connects_to_driver(plugin):
+    from bigchaindb_driver import BigchainDB
+    assert isinstance(plugin.driver, BigchainDB)
 
 
-class TestPlugin(object):
+def test_create_user(plugin):
+    user = plugin.create_user()
+    assert isinstance(user.verifying_key, str)
+    assert isinstance(user.signing_key, str)
 
-    @classmethod
-    def setup_class(cls):
-        pass
 
-    def test_something(self):
-        pass
+@mark.parametrize(
+    'model_name',
+    ('manifestation_model_jsonld', 'manifestation_model_json'))
+def test_save_model_jsonld(plugin, bdb_driver, model_name, alice_keypair,
+                           request):
+    model_data = request.getfixturevalue(model_name)
+    tx_id = plugin.save(model_data, user=alice_keypair)
 
-    @classmethod
-    def teardown_class(cls):
-        pass
+    # Sleep to give BigchainDB some time to process the transaction
+    sleep(5)
+
+    tx = bdb_driver.transactions.retrieve(tx_id)
+    tx_payload = tx['transaction']['data']['payload']
+    tx_new_owners = tx['transaction']['conditions'][0]['owners_after']
+    assert tx['id'] == tx_id
+    assert tx_payload == model_data
+    assert tx_new_owners[0] == alice_keypair.verifying_key
+
+
+@mark.skip(reason='get_status() not implemented yet')
+def test_get_model_status(plugin, persisted_manifestation):
+    status = plugin.get_status(persisted_manifestation['id'])
+    assert status in (None, 'valid', 'invalid', 'undecided', 'backlog')
+
+
+@mark.skip(reason='transfer() not implemented yet')
+def test_transfer(plugin, bdb_driver, persisted_manifestation,
+                  alice_keypair, bob_keypair):
+    tx_id = persisted_manifestation['id']
+
+    transfer_tx_id = plugin.transfer(tx_id,
+                                     from_user=alice_keypair,
+                                     to_user=bob_keypair)
+
+    # Sleep to give BigchainDB some time to process the transaction
+    sleep(5)
+
+    transfer_tx = bdb_driver.transactions.retrieve(transfer_tx_id)
+    transfer_tx_fulfillments = transfer_tx['transaction']['fulfillments']
+    transfer_tx_conditions = transfer_tx['transaction']['conditions']
+    transfer_tx_prev_owners = transfer_tx_fulfillments[0]['owners_before']
+    transfer_tx_new_owners = transfer_tx_conditions[0]['owners_after']
+    assert transfer_tx['id'] == tx_id
+    assert transfer_tx_prev_owners[0] == alice_keypair.verifying_key
+    assert transfer_tx_new_owners[0] == bob_keypair.verifying_key
+
+
+# TODO: add error case tests
