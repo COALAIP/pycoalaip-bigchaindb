3b39b6c4f8df46fcddb8463df18207085e4bc7ee
Brett Sun <qisheng.brett.sun@gmail.com>
2016-09-28 18:35:15 +0200

    cosmetic(test): reorganize tests (#12)

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-from time import sleep
-from pytest import mark, fail, raises
+from pytest import mark, raises
+from tests.utils import bdb_transaction_test, poll_result
 
 
 def test_plugin_type_is_bigchaindb(plugin):
@@ -23,15 +23,14 @@ def test_generate_user(plugin):
     'manifestation_model_jsonld',
     'manifestation_model_json'
 ])
-def test_save_model_jsonld(plugin, bdb_driver, model_name, alice_keypair,
-                           request):
+def test_save_model(plugin, bdb_driver, model_name, alice_keypair, request):
     model_data = request.getfixturevalue(model_name)
     tx_id = plugin.save(model_data, user=alice_keypair)
 
     # Poll BigchainDB for the result
-    tx = _poll_result(
+    tx = poll_result(
         lambda: bdb_driver.transactions.retrieve(tx_id),
-        _bdb_transaction_test
+        bdb_transaction_test
     )
 
     tx_payload = tx['transaction']['data']['payload']
@@ -55,11 +54,12 @@ def test_save_raises_on_error(monkeypatch, plugin, manifestation_model_json,
         plugin.save(manifestation_model_json, user=alice_keypair)
 
 
-def test_get_model_status(plugin, bdb_driver, persisted_manifestation):
+def test_get_persisted_model_status(plugin, bdb_driver,
+                                    persisted_manifestation):
     tx_id = persisted_manifestation['id']
 
     # Poll BigchainDB for the initial status
-    _poll_result(
+    poll_result(
         lambda: plugin.get_status(tx_id),
         lambda result: result['status'] in (
             'valid', 'invalid', 'undecided', 'backlog')
@@ -68,7 +68,7 @@ def test_get_model_status(plugin, bdb_driver, persisted_manifestation):
     # Poll BigchainDB until the transaction validates; will fail test if the
     # transaction's status doesn't become valid by the end of the timeout
     # period.
-    _poll_result(
+    poll_result(
         lambda: plugin.get_status(tx_id),
         lambda result: result['status'] == 'valid'
     )
@@ -103,9 +103,9 @@ def test_transfer(plugin, bdb_driver, persisted_manifestation, model_name,
                                      to_user=bob_keypair)
 
     # Poll BigchainDB for the result
-    transfer_tx = _poll_result(
+    transfer_tx = poll_result(
         lambda: bdb_driver.transactions.retrieve(transfer_tx_id),
-        _bdb_transaction_test
+        bdb_transaction_test
     )
 
     transfer_tx_fulfillments = transfer_tx['transaction']['fulfillments']
@@ -115,37 +115,3 @@ def test_transfer(plugin, bdb_driver, persisted_manifestation, model_name,
     assert transfer_tx['id'] == tx_id
     assert transfer_tx_prev_owners[0] == alice_keypair['verifying_key']
     assert transfer_tx_new_owners[0] == bob_keypair['verifying_key']
-
-
-def _poll_result(fn, result_test_fn, *, max_checks=5, interval=1):
-    """Polling utility for cases where we need to wait for BigchainDB
-    processing. After 'max_checks' attempts, will fail the test with the
-    last result.
-
-    Args:
-        fn (func): polling function to invoke
-        result_test_fn (func): test function to validate the result of
-            the polling function; return true if the result is valid and
-            can be returned
-        max_checks (int): maximum poll attempts before failing test
-        interval (num): interval between each poll attempt
-
-    Returns:
-        (any): the result of 'fn' if it passed validation
-    """
-    for _ in range(max_checks):
-        try:
-            result = fn()
-        except Exception:
-            # Just fail this polling instance and try again
-            pass
-        else:
-            if result_test_fn(result):
-                return result
-        sleep(interval)
-
-    fail("Polling result failed with result: '{}'".format(result))
-
-
-def _bdb_transaction_test(tx_result):
-    return tx_result.get('status') != 404 and tx_result.get('id')
