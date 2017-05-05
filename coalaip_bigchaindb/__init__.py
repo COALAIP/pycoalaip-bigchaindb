acd2625540b0acd6776708263e44aa9a9f6bb5fc
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2017-05-05 19:13:50 +0200

    Increase version to 0.0.3 [skip ci]

--- coalaip_bigchaindb/__init__.py
+++ coalaip_bigchaindb/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'BigchainDB'
 __email__ = 'dev@bigchaindb.com'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from coalaip_bigchaindb.plugin import Plugin  # noqa
--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ docs_require = [
 
 setup(
     name='coalaip-bigchaindb',
-    version='0.0.2',
+    version='0.0.3',
     description="BigchainDB ledger plugin for COALA IP's Python reference implementation",
     long_description=readme + '\n\n' + history,
     author="BigchainDB",
