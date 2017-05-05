176fc4b566ae45cd4d8f69e5e8f82ffb2c6973e5
Sylvain Bellemare <sbellem@gmail.com>
2017-05-05 16:31:56 +0200

    Increase version to 0.0.2 [skip ci] (#45)

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ docs_require = [
 
 setup(
     name='coalaip-bigchaindb',
-    version='0.0.1',
+    version='0.0.2',
     description="BigchainDB ledger plugin for COALA IP's Python reference implementation",
     long_description=readme + '\n\n' + history,
     author="BigchainDB",
